7011693e104ab116994367d17ae1d0475dfb6608
Ecaterina Pihurov <ecaterinaoprea92@gmail.com>
2021-03-17 14:59:30 +0200

    First Commit

---
+++ kelvin.js
@@ -0,0 +1,12 @@
+const kelvin = 293;// I set a constant variable to kelvin
+
+let celsius = kelvin - 273;// Celsius is Kelvin-273
+
+let fahrenheit = celsius * (9/5) + 32;// Converting Celsius to Fahrenheit
+
+
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit`);
+
+
